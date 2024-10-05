bc4fc6de894a97b8f8db30d7af43bde7b1a5f35b
uenri <luwenli1031@gmail.com>
2024-10-05 22:20:09 +0800

    0920網頁上

--- bmi.js
+++ bmi.js
@@ -1,5 +1,14 @@
-var weight = 50;
-var height = 160;
-var bmi = weight/((height/100)**2);
 
-console.log("Hello! You bmi is "+bmi);
+
+function calcBMI(){
+
+    //var weight = 50;
+    var weight = Number(document.getElementById("weight").value);
+    //var height = 160;
+    var height = Number(document.getElementById("height").value);
+    var bmi = weight/((height/100)**2);
+
+    document.getElementById("BMI").innerHTML = bmi;
+    
+    console.log("Hello! You bmi is "+bmi);
+}
