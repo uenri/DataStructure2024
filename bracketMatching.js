6e3598d9572bc60625e1bb679fdacd51a4524035
uenri <luwenli1031@gmail.com>
2024-12-19 23:23:22 +0800

    113

---
+++ bracketMatching.js
@@ -0,0 +1,35 @@
+function bracketMatching(str){
+    var left="{[(";
+    var right="}])";
+    var open=[] //stack
+    var symbol = str.charAt(i=0);
+    var isMatched = true;
+    while(isMatched && symbol != ''){
+        //if(symbol =='{' || symbol=='(' || symbol == '['){
+        if(left.includes(symbol)){
+            open.push(symbol);
+        }
+        //if(symbol=='}' || symbol==')' || symbol==']'){
+        if(right.includes(symbol)){
+            if(open.length==0)
+                isMatched=false;
+            else{
+                match = open.pop();
+                //isMatched = (symbol=='}' && match=='{') ||   (symbol==')' && match=='(') || (symbol==']' && match=='[');
+                isMatched = left.indexOf(match) == right.indexOf(symbol);
+            } 
+        }
+       // i++;//++i
+        symbol = str.charAt(++i);
+    }  
+    if(open.length>0 || !isMatched)
+        return 'unMatched';
+    else
+        return 'matched';
+}
+
+
+var test="a = (1 + v(b[3 + c[4]]))"
+console.log(bracketMatching(test));
+
+ 
